e4fe891417ad226c9d571353abeef11aee2401e2
bekhzodx64 <mr.bekzod2018@gmail.com>
2025-08-07 22:29:56 +0500

    fix: intro section

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
+import ContactForm from '@/components/sections/contact-form'
+import Download from '@/components/sections/download'
 import Info from '@/components/sections/info'
+import Intro from '@/components/sections/intro'
+import Plans from '@/components/sections/plans'
+import Prices from '@/components/sections/prices'
+import Questions from '@/components/sections/questions'
 
 export default function Home() {
 	return (
 		<>
-			{/* <Intro /> */}
-			{/* <Prices /> */}
-			{/* <Plans /> */}
+			<Intro />
+			<Prices />
+			<Plans />
 			<Info />
-			{/* <ContactForm /> */}
-			{/* <Questions /> */}
-			{/* <Download /> */}
+			<ContactForm />
+			<Questions />
+			<Download />
 		</>
 	)
 }
